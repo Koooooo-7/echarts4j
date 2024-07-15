e2653c238be3c78d2760f9ebbf841b7a4edca380
Koy Zhuang <koy@ko8e24.top>
2024-07-15 19:56:11 +0800

    fix: ci

--- echarts4j-tpl-spec/src/main/java/com/github/koooooo7/echarts4j/tpl/spec/Echarts4jC.java
+++ echarts4j-tpl-spec/src/main/java/com/github/koooooo7/echarts4j/tpl/spec/Echarts4jC.java
@@ -4,7 +4,6 @@ import com.github.koooooo7.echarts4j.chart.Canvas;
 import com.github.koooooo7.echarts4j.tpl.spec.core.Echarts4jDefListener;
 import com.github.koooooo7.echarts4j.tpl.spec.parsing.Echarts4jLexer;
 import com.github.koooooo7.echarts4j.tpl.spec.parsing.Echarts4jParser;
-import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -14,14 +13,11 @@ import java.io.InputStream;
 
 public class Echarts4jC {
     public static Canvas parse(InputStream is) throws IOException {
-        final CharStream cs = CharStreams.fromStream(is);
-        final Echarts4jLexer lexer = new Echarts4jLexer(cs);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CommonTokenStream tokens = new CommonTokenStream(new Echarts4jLexer(CharStreams.fromStream(is)));
         final Echarts4jParser echarts4jParser = new Echarts4jParser(tokens);
-        final var svc = echarts4jParser.start();
         final ParseTreeWalker parseTreeWalker = new ParseTreeWalker();
         final Echarts4jDefListener echarts4jDefListener = new Echarts4jDefListener();
-        parseTreeWalker.walk(echarts4jDefListener, svc);
+        parseTreeWalker.walk(echarts4jDefListener, echarts4jParser.start());
         return echarts4jDefListener.get();
 
     }
