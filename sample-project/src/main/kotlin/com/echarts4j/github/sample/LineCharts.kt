2cf3e468fcb8f69aa230695ee42e5576506a33c9
Koy Zhuang <koy@ko8e24.top>
2024-07-15 20:17:13 +0800

    chore(sample): sync to v1.0.0

--- sample-project/src/main/kotlin/com/echarts4j/github/sample/LineCharts.kt
+++ sample-project/src/main/kotlin/com/echarts4j/github/sample/LineCharts.kt
@@ -3,10 +3,14 @@ package com.echarts4j.github.sample
 import com.github.koooooo7.echarts4j.chart.Canvas
 import com.github.koooooo7.echarts4j.chart.LineChart
 import com.github.koooooo7.echarts4j.option.ChartOption
+import com.github.koooooo7.echarts4j.option.chart.Legend
+import com.github.koooooo7.echarts4j.option.chart.Legend.LegendBuilder
 import com.github.koooooo7.echarts4j.option.chart.Title
 import com.github.koooooo7.echarts4j.option.chart.Toolbox
 import com.github.koooooo7.echarts4j.option.chart.Toolbox.SaveAsImage
+import com.github.koooooo7.echarts4j.option.series.line.Label
 import com.github.koooooo7.echarts4j.option.series.line.LineChartSeries
+import com.github.koooooo7.echarts4j.type.FuncStr
 import java.io.File
 
 
